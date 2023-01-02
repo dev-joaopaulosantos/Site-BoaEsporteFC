160d6c0bcccda9876d4cdf24ddf090fcc7309550
João Paulo Dos Santos Silva <83318297+Git-JoaoPauloSantos@users.noreply.github.com>
2023-01-02 00:00:00 -0300

    Update FooterContainer.jsx

--- src/components/FooterContainer.jsx
+++ src/components/FooterContainer.jsx
@@ -4,7 +4,7 @@ const FooterContainer = () => {
   return (
     <footer id="footer-container">
         <h2>Boa Esporte Futebol Clube</h2>
-        <p>Lagoa De são Francisco / Piauí</p>
+        <p>Lagoa De são Francisco - Piauí</p>
         <p>2022 - Todos os direitos reservados</p>
     </footer>
   )
